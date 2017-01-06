ca118cac702a69dd7d80268f41bf8cd3eec24890
Yu Chen <yu.chen@ozstrategy.com>
2017-01-06 17:01:38 +0800

    add ext

---
+++ cms-web/src/main/java/org/ychen/cms/controller/ExtController.java
@@ -0,0 +1,13 @@
+package org.ychen.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by cy on 17/1/6.
+ */
+@Controller
+@RequestMapping("/ext")
+public class ExtController {
+
+}
