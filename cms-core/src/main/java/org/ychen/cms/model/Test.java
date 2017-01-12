82c4c5e28f72ae2891f0402301d3f28c88a25d2e
Yu Chen <yu.chen@ozstrategy.com>
2017-01-12 11:35:01 +0800

    add module review

---
+++ cms-core/src/main/java/org/ychen/cms/model/Test.java
@@ -0,0 +1,7 @@
+package org.ychen.cms.model;
+
+/**
+ * Created by cy on 17/1/11.
+ */
+public class Test {
+}
---
+++ cms-web/src/main/java/org/ychen/cms/dto/RoleCommand.java
@@ -0,0 +1,7 @@
+package org.ychen.cms.dto;
+
+/**
+ * Created by cy on 17/1/10.
+ */
+public class RoleCommand {
+}
